f330d20039ef07b4cfc1b4d00bcbe98451685501
SYSTEM-VD-ZDY <334063894@qq.com>
2024-12-08 14:56:32 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var sidebar = document.getElementById('sidebar');
-    if (sidebar.style.display === 'none') {
-        sidebar.style.display = 'block';
-    } else {
-        sidebar.style.display = 'none';
-    }
-}
